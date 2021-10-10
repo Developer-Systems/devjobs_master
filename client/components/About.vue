f77d65e9a57f9ddcf3b871e4fc0e4cabe5ed5191
John Andrade <johnandrade18@hotmail.com>
2021-10-10 14:57:24 -0500

    Mejoras componentes del home

--- client/components/About.vue
+++ client/components/About.vue
@@ -1,169 +1,156 @@
 <template>
-    <div class="container">
-        <!--::before-->
-        <!--<div class="log area_p">
+  <div class="information_content">
+    <section class="content">
+      <h1 class="companyName text-dark">DevJobs</h1>
+      <h1 class="slogan text-dark">Conectando talentos y oportunidades</h1>
 
-            <img class="logo" src="" alt="Imagen DejJobs">
-        </div>-->
-        <div class="information_content">
-        <section class="content">
-            <h1 class="companyName text-white">DevJobs</h1>
-            <h1 class="slogan text-white-50">Conectando talentos y oportunidades</h1>
+      <div class="section text-dark">
+        <article class="article">
+          DevJobs es una aplicación Web, que tiene como propósito ser el puente
+          entre los mejores desarrolladores y las oportunidades laborales más
+          interesantes. Queremos contribuir positivamente a la sociedad,
+          ayudando a los emprendedores que requieren el mejor talento para
+          adelantar sus proyectos y a los desarrolladores a conectarse con los
+          empleos más llamativos, nuestra visión poder conectar al mundo.
+        </article>
+        <br />
 
-            <div class="section text-white-50">
-            <article class="article">
-                DevJobs es una aplicación Web, que tiene como propósito ser el puente entre  
-                los mejores desarrolladores y las oportunidades laborales más interesantes. 
-                Queremos contribuir positivamente a la sociedad, ayudando a los emprendedores 
-                que requieren el mejor talento para adelantar sus proyectos y a los desarrolladores a conectarse 
-                con los empleos más llamativos, nuestra visión poder conectar al mundo.
-                
+        <article class="article">
+          Somos concientes de la existencia de diversas plataformas de empleo,
+          sin embargo nos destacamos porque somo una comunidad especializada en
+          el talento en desarrollo de software y estamos a la vanguardia de las
+          tendencias y de las tecnologías, ofreciendo la oportunidad a las
+          personas que han capaticado de ser visibles en el mercado laboral.
+        </article>
+        <br />
 
-            </article>
-            <br> 
+        <article class="article">
+          En DevJobs hemos entendido que el papel de los desarrolladores en la
+          sociedad es muy importante, en este mundo globalizado, tenemos la
+          responsabilidad de atender a las crecientes necesidades de expasión de
+          todos los sectores: la economía, la educación, la salud, la cultura,
+          el deporte, la agricultura no existe un campo en donde no se involucre
+          la tecnología, y por eso queremos ser el canal que facilite este
+          proceso.
+        </article>
 
-            <article class="article">
-                Somos concientes de la existencia de diversas plataformas de empleo, sin embargo nos destacamos porque
-                somo una comunidad especializada en el talento en desarrollo de software y estamos a la vanguardia de las 
-                tendencias y de las tecnologías, ofreciendo la oportunidad a las personas que han capaticado de ser visibles
-                en el mercado laboral.
+        <br />
+        <article class="article">
+          DevJobs es un sitio web para emprendedores hecho por emprendedores,
+          los integrantes de este equipo nos desenvolvemos en diferentes
+          disciplinas y aportamos nuestros conocimientos a la comunidad para
+          generar un espacio intuitivo y dinámico. Hacemos parte de la
+          <mark><b>MisiónTic UTP 2022</b></mark>
 
-            </article>
-            <br>
+          <p></p>
 
-            <article class="article">
-                En DevJobs hemos entendido que el papel de los desarrolladores en la sociedad es muy importante, 
-                en este mundo globalizado, tenemos la responsabilidad de atender a las crecientes necesidades 
-                de expasión de todos los sectores: la economía, la educación, la salud, la cultura, el deporte, la agricultura 
-                no existe un campo en donde no se involucre la tecnología, y  por eso queremos ser el canal
-                que facilite este proceso. 
+          <div class="contact">
+            <p>
+              John Alexander Andrade Díaz <br />
+              <a href="#" class="text-white-50">sistemas@johnandrade.com</a>
+            </p>
 
-            </article>
+            <p>
+              Juan Felipe Tamayo Ospina <br />
+              <a href="#" class="text-white-50">tamayo998@hotmail.com</a>
+            </p>
 
-            <br>
-            <article class="article">
-                DevJobs es un sitio web para emprendedores hecho por emprendedores, los integrantes de este equipo
-                nos desenvolvemos en diferentes disciplinas y aportamos nuestros conocimientos a la comunidad
-                para generar un espacio intuitivo y dinámico. Hacemos parte de la <mark><b>MisiónTic UTP 2022</b></mark>
-                
-               <p>
+            <p>
+              Juan Pablo Aguirre Martinez <br />
+              <a href="#" class="text-white-50">jpaguirre@unal.edu.com</a>
+            </p>
 
-                <div class="contact">
-                    
-                        <p>John Alexander Andrade Díaz <br>  
-                        <a href="#" class="text-white-50">sistemas@johnandrade.com</a>  
-                        </p>
+            <p>
+              Oscar Santiago Pinilla Caicedo <br />
+              <a href="#" class="text-white-50"
+                >santiago.pinilla.45672@gmail.com</a
+              >
+            </p>
 
-                        <p>Juan Felipe Tamayo Ospina <br>
-                        <a href="#" class="text-white-50">tamayo998@hotmail.com</a>  
-                        </p>
-
-                        <p>Juan Pablo Aguirre Martinez <br>
-                        <a href="#" class="text-white-50">jpaguirre@unal.edu.com</a>  
-                        </p>
-
-                        <p>Oscar Santiago Pinilla Caicedo <br>
-                        <a href="#" class="text-white-50">santiago.pinilla.45672@gmail.com</a>  
-                        </p>
-
-                        <p>Viviana Castelblanco Fajardo <br> 
-                        <a href="#" class="text-white-50">vivi.c11@hotmail.com</a>
-                        </p> 
-                </div>
-            
-            </article>
-            </div>
-        </section>
-        
-
-        <aside class="aside_image">
-            <img src="Copia.png" class="img-responsive" alt="DejJobs">
-        </aside>
-        </div>
-    
-    </div>
+            <p>
+              Viviana Castelblanco Fajardo <br />
+              <a href="#" class="text-white-50">vivi.c11@hotmail.com</a>
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
-*{
-    margin: 0;
-    padding:0;
-}
-
-@font-face {
-    font-family: SloganFont;
-    src: url(Crystal_symphony.ttf);
-
+* {
+  margin: 0;
+  padding: 0;
 }
 
 .companyName {
-    font-family: "PoppinsBold.ttf";
-    
-  }
-
-h1{
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  font-family: 'Poppins', sans-serif;
 }
 
-.display-1{
-    src: url(PoppinsBold.ttf);
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-body{
-    background: #1A1A1A;
-}
-.slogan{
-    font-family:SloganFont;
+.display-1 {
+  font-family: 'Poppins', sans-serif;
 }
 
-.container{
-    width: 1200px;
-    height: 1000px;
-    margin: 0.5em auto;
-    padding: 0.5em;
+body {
+  background: #1a1a1a;
+}
+.slogan {
+  font-family: 'Crystal symphony Personal use only', sans-serif;
 }
 
-.information_content{
-    height: 40%;
+.container {
+  width: 1200px;
+  height: 1000px;
+  margin: 0.5em auto;
+  padding: 0.5em;
 }
 
-.section{
-    height: 95%;
-    width: 70%;
-    float: left;
-    margin: 0.6em;
+.information_content {
+  height: 40%;
 }
 
-.aside_image{
-    height: 95%;
-    width: 25%;
-    float: right;
-    margin: 0.6em;
+.section {
+  height: 95%;
+  width: 70%;
+  float: left;
+  margin: 0.6em;
 }
 
-.aside_image{
-    display: grid;
-    align-items: center;
-    justify-content: center;
+.aside_image {
+  height: 95%;
+  width: 25%;
+  float: right;
+  margin: 0.6em;
 }
 
-.article{
-    width: 90%;
-    height: 20%;
-    margin:  1em;
+.aside_image {
+  display: grid;
+  align-items: center;
+  justify-content: center;
 }
 
-.contact{
-    display: grid;
-    align-items: center;
-    justify-content: center;
+.article {
+  width: 90%;
+  height: 20%;
+  margin: 1em;
 }
 
-.end{
-    display: grid;
-    align-items: center;
-    justify-content: center;
+.contact {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+}
+
+.end {
+  display: grid;
+  align-items: center;
+  justify-content: center;
 }
 </style>
